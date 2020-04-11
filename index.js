067f94886854b51408debb58da65adec54e25d77
Gustavo Santana Domingueti <gustavo.s.domingueti@gmail.com>
2020-04-11 16:59:19 -0300

    Projeto feito em electron para testar obter metados de um video especifico utilizando ffmpeg e fluent-ffmpeg

---
+++ index.js
@@ -0,0 +1,23 @@
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+
+        mainWindow.webContents.send('video:metadata', metadata.format.duration);
+    });
+});
